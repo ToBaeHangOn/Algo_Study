d59ca5519aaede54d39a86d23ceb038f7c4e9b34
ywoo12321 <ywoo12321@gmail.com>
2023-01-19 18:20:04 +0900

    problem2 해결 못함 소요시간 2시간

---
+++ 230118/problem2.js
@@ -0,0 +1,34 @@
+// const fs = require("fs");
+// let [n, k] = fs.readFileSync("./dev/stdin").toString().split(" ");
+k = Number(17);
+n = Number(5);
+[fastest, way] = [10 ** 6, 0];
+const q = [];
+
+const BFS = (n) => {
+  q.push([n, 0]);
+  const visit = new Array(10001).fill(false);
+  visit[n] = true;
+  while (q) {
+    [now, time] = q.shift();
+    // 똑같은 지점에 도달하는 방법이 여러개 존재할 수 있으므로 pop한 후 방문 여부 확인
+    visit[now] = true;
+    // 똑같은 시간대에 동생 위치에 또 도착
+    if (now === k && time <= fastest) {
+      fastest = min(fastest, time);
+      ways += 1;
+    }
+    if (time > fastest) {
+      break; // 큐에는 시간 순으로 들어가므로 fastest보다 커지면 탐색을 종료
+    }
+
+    for (x of [now - 1, now + 1, now * 2]) {
+      if (x <= 10001 && x > 0 && visit[x] === false) {
+        q.push((x, time + 1));
+      }
+    }
+  }
+};
+BFS(n);
+console.log(fastest);
+console.log(way);
