1ef340640a87cdd1334c6a3dc390efc8d5a6dff5
문석희 <zazachucky@naver.com>
2023-03-04 23:09:58 +0900

    Finally : boj_24432_카드 놀이

--- #15 230301/boj_24432_카드 놀이/Solution.java
+++ #15 230301/boj_24432_카드 놀이/Solution.java
@@ -3,20 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Solution {
     static int n, m, k;
     static boolean[] visited;
 
-    static int[] arr;
+//    static int[] arr;
     //    static int[] results;
-    static int[] selected;
+//    static int[] selected;
     //    static Stack<Integer> stack;
-    static Set<Integer> set;
-    static int[] bobList;
-    static int[] aliceList;
+//    static HashSet<Integer> set;
+//    static int[] bobList;
+//    static int[] aliceList;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,38 +26,43 @@ public class Solution {
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
-            arr = new int[n];
+            int[] arr = new int[n];
+//            int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < n; i++) {
                 int temp = Integer.parseInt(st.nextToken());
                 arr[i] = temp;
             }
             visited = new boolean[n];
-            set = new HashSet<>();
+            HashSet<Integer> set = new HashSet<>();
 //            stack = new Stack<>();
-            selected = new int[k];
-            rec(0, n);
-            bobList = new int[set.size()];
-            int index = 0;
-            for (Integer i : set)
-                bobList[index++] = i;
+//            selected = new int[k];
+//            rec(0, n, set, 0);
+            dfs(0, arr, set, 0, 0);
+            int[] bobList = set.stream().mapToInt(Integer::intValue).toArray();
+//            int index = 0;
+//            for (Integer i : set)
+//                bobList[index++] = i;
 
             //alice
+//            arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             arr = new int[m];
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < m; i++) {
                 int temp = Integer.parseInt(st.nextToken());
                 arr[i] = temp;
             }
-            visited = new boolean[m];
+//            visited = new boolean[m];
 //            stack = new Stack<>();
-            set = new HashSet<>();
-            selected = new int[k];
-            rec(0, m);
-            aliceList = new int[set.size()];
-            index = 0;
-            for (Integer i : set)
-                aliceList[index++] = i;
+            set.clear();
+//            selected = new int[k];
+//            rec(0, m, set, 0);
+            dfs(0, arr, set, 0, 0);
+            int[] aliceList = set.stream().mapToInt(Integer::intValue).toArray();
+            ;
+//            index = 0;
+//            for (Integer i : set)
+//                aliceList[index++] = i;
             Arrays.sort(aliceList);
             Arrays.sort(bobList);
             int min = Integer.MAX_VALUE, max = 0;
@@ -66,12 +70,13 @@ public class Solution {
             int indexBob = 0;
             int indexAlice = 0;
 
-            // 원하는 최솟값 탐색
             while (indexBob < bobList.length && indexAlice < aliceList.length) {
                 int temp = Math.abs(bobList[indexBob] - aliceList[indexAlice]);
                 if (temp < min) {
                     min = temp;
-                } else if (bobList[indexAlice] <= aliceList[indexBob])
+                }
+
+                if (bobList[indexBob] <= aliceList[indexAlice])
                     indexBob++;
                 else
                     indexAlice++;
@@ -109,25 +114,78 @@ public class Solution {
         System.out.println(sb);
     }
 
-    static void rec(int cnt, int limit) {
-//        System.out.println("rec : " + limit);
-        if (cnt == k) {
-            int temp = 0;
-            for (int i : selected)
-                temp += i;
-            set.add(temp);
+    public static void dfs(int i, int[] arr, HashSet<Integer> set, int sum, int count) {
+        if (count > k) {
             return;
         }
-
-        for (int i = 0; i < limit; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                selected[cnt] = arr[i];
-                rec(cnt + 1, limit);
-                visited[i] = false;
-            }
+        if (count == k) {
+            set.add(sum);
+            return;
         }
+        if (i == arr.length) {
+            return;
+        }
+        dfs(i + 1, arr, set, sum, count);
+        dfs(i + 1, arr, set, sum + arr[i], count + 1);
     }
+
+    //    static void dfs(int i, int[] cards, HashSet<Integer> set, int sum, int count) {
+//        if (count > k) {
+//            return;
+//        } else if (count == k) {
+//            set.add(sum);
+//            return;
+//        } else if (i == cards.length) {
+//            return;
+//        } else {
+//            dfs(i + 1, cards, set, sum, count);
+//            dfs(i + 1, cards, set, sum + cards[i], count + 1);
+//        }
+//    }
+//    public static void dfs(int i, int[] cards, HashSet<Integer> set, int sum, int count) {
+//        switch (count) {
+//            case 0:
+//            case 1:
+//            case 2:
+//            case 3:
+//            case 4:
+//            case 5:
+//            case 6:
+//            case 7:
+//            case 8:
+//            case 9:
+//            case 10:
+//                if (count == k) {
+//                    set.add(sum);
+//                }
+//                if (i < cards.length) {
+//                    dfs(i + 1, cards, set, sum, count);
+//                    dfs(i + 1, cards, set, sum + cards[i], count + 1);
+//                }
+//                break;
+//            default:
+//                break;
+//        }
+//    }
+
+//    static void rec(int cnt, int limit, HashSet<Integer> set, int sum) {
+////        System.out.println("rec : " + limit);
+//        if (cnt == k) {
+////            int temp = 0;
+////            for (int i : selected)
+////                temp += i;
+//            set.add(sum);
+//            return;
+//        }
+//
+//        for (int i = 0; i < limit; i++) {
+//            if (!visited[i]) {
+//                visited[i] = true;
+//                rec(cnt + 1, limit, set, sum + arr[i]);
+//                visited[i] = false;
+//            }
+//        }
+//    }
 
 
 }
